07c16c9f7fadbe0ecb378aed31da2418758e2218
Seungsoo Kim <45951286+SSKim86@users.noreply.github.com>
2020-02-27 22:47:29 -0500

    Update postgwas-exe.r

--- postgwas-exe.r
+++ postgwas-exe.r
@@ -4,13 +4,13 @@
 help_message = c('
 Version: 2020-02-20
 
-Usage: Rscript postgwas-exe.r [Functions]
+Usage: Rscript postgwas-exe.r <Function calls>
     <--base file(s)> <--out folder> <options> <--debug>
     
-Functions:
-    --gwas    This is a function for GWAS Catalog data.
-    --ldlink  This is a function for LDlink data.
-    --bw2bed  This is a function to convert BigWig to BED format.
+Function calls:
+    --gwas    This is a function call for GWAS Catalog data.
+    --ldlink  This is a function call for LDlink data.
+    --dbdown  This is a function call for downloading databases.
 
 Global arguments:
     --base    Base input file is mendatory.
@@ -22,10 +22,6 @@ Running functions with --help or -h arguments prints usage information for the [
 
 # Command Input ----------
 commandLine = commandArgs(trailingOnly=T)
-help   = (sum(c('--help')   %in% commandLine[1])>0)
-gwas   = (sum(c('--gwas')   %in% commandLine[1])>0)
-ldlink = (sum(c('--ldlink') %in% commandLine[1])>0)
-
 comm = paste(unlist(commandLine),collapse=' ')
 listoptions = unlist(strsplit(comm,'--'))[-1]
 args = lapply(listoptions,function(x) {
@@ -33,21 +29,24 @@ args = lapply(listoptions,function(x) {
     if(length(arg)==0) { return(TRUE)
     } else return(arg)
 })
-args.names = sapply(listoptions,function(x) {
+args_names = sapply(listoptions,function(x) {
     option = unlist(strsplit(x,' '))[1]
 })
-names(args) = unlist(args.names)
+args_names = unlist(args_names)
+names(args) = args_names
 
 # Run Function ----------
-if(help) { cat(help_message)
+call = args_names[1]
+if(call=='help') { cat(help_message)
+} else if(call=='gwas') {
+    source('src/gwas_catalog.r')
+    gwas_catalog(args)
+} else if(call=='ldlink') {
+    source('src/gwas_ldlink.r')
+    gwas_ldlink(args)
+} else if(call=='dbdown') {
+    source('src/db_download.r')
+    db_download(args)
 } else {
-    if(gwas) {
-        source('src/gwas_catalog.r')
-        gwas_catalog(args)
-    } else if (ldlink) {
-        source('src/gwas_ldlink.r')
-        gwas_ldlink(args)
-    } else {
-        paste0('There is no such function: "',commandLine[1],'". Try again.')
-    }
+    paste0('There is no such function: "',commandLine[1],'". Try again.')
 }
