8d15be9f7be7730fe9bbf0bdb9d2a4b50b603eda
Seungsoo Kim <45951286+SSKim86@users.noreply.github.com>
2020-09-03 23:59:36 -0400

    Update snippet-sqlite.r

--- src/snippet-sqlite.r
+++ src/snippet-sqlite.r
@@ -1,14 +1,14 @@
 # setwd('/data')
+# ref: https://www.datacamp.com/community/tutorials/sqlite-in-r
 
 library(dplyr)
 library(RSQLite)
 
-# Read gtex file
-gtex = readRDS('db_gwas/gtex_signif_5e-08_Ensgid_dt.rds')
-dim(gtex) %>% print
+f_rds = 'db_gwas/gtex_signif_5e-08_Ensgid_dt.rds'
 
-# Convert data.table to df
-gtex  = gtex %>% as.data.frame
+# Read gtex file
+gtex = readRDS(f_rds) %>% as.data.frame
+dim(gtex) %>% print
 
 # Generate gtex.db file
 conn = dbConnect(RSQLite::SQLite(),'db_gwas/db_gwas.db')
@@ -16,3 +16,6 @@ dbWriteTable(conn,"gtex",gtex)
 
 # Check db list
 dbListTables(conn)
+
+# Close the database connection
+dbDisconnect(conn)
