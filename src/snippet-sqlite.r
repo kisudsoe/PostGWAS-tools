87394ad7754f6b8950e15e4a5e71f9582395e537
Seungsoo Kim <kisudsoe@gmail.com>
2020-09-08 00:32:07 -0400

    Update v0.2

--- src/snippet-sqlite.r
+++ src/snippet-sqlite.r
@@ -5,10 +5,7 @@ library(dplyr)
 library(RSQLite)
 
 f_rds = 'db_gwas/gtex_signif_5e-08_Ensgid_dt.rds'
-<<<<<<< HEAD
 f_db = 'db_gwas/db_gwas.db'
-=======
->>>>>>> 6cc62cfa7e5126607873a865a511af29b258b319
 
 # Read gtex file
 gtex = readRDS(f_rds) %>% as.data.frame
@@ -20,12 +17,7 @@ dbWriteTable(conn,"gtex",gtex)
 
 # Check db list
 dbListTables(conn)
-<<<<<<< HEAD
-
-# Disconnect DB
-=======
 dbGetQuery(conn, "SELECT * FROM gtex LIMIT 10")
 
 # Close the database connection
->>>>>>> 6cc62cfa7e5126607873a865a511af29b258b319
 dbDisconnect(conn)
